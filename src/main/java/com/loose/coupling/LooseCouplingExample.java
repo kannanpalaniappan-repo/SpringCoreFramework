4263f0d42a9042a3afd844f37ec28f1819e1b6f6
kannanpalaniappan-repo <kannanpalaniappan2001@gmail.com>
2025-04-04 07:56:22 +0530

    Challenge: Inversion of Control(IOC)

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,17 +1,24 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserDataProvider userDataProvider = new UserDatabaseProvider();
-        UserManager userManagerWithDB = new UserManager(userDataProvider);
-        System.out.println(userManagerWithDB.getUserInfo());
+//        UserDataProvider userDataProvider = new UserDatabaseProvider();
+//        UserManager userManagerWithDB = new UserManager(userDataProvider);
+//        System.out.println(userManagerWithDB.getUserInfo());
+//
+//        UserDataProvider webServiceDataProvider = new WebServiceDataProvider();
+//        UserManager userManagerWithWS = new UserManager(webServiceDataProvider);
+//        System.out.println(userManagerWithWS.getUserInfo());
+//
+//        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
+//        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+//        System.out.println(userManagerWithNewDB.getUserInfo());
 
-        UserDataProvider webServiceDataProvider = new WebServiceDataProvider();
-        UserManager userManagerWithWS = new UserManager(webServiceDataProvider);
-        System.out.println(userManagerWithWS.getUserInfo());
-
-        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
-        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
-        System.out.println(userManagerWithNewDB.getUserInfo());
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationLooseCoupling.xml");
+        UserManager userManager = (UserManager) context.getBean("userManager");
+        System.out.println(userManager.getUserInfo());
     }
 }
